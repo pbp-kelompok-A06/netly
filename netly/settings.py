98893552f18e706bb464bb2dba0e22da022c64ba
Shotheaven <evan@Shotheavens-MacBook-Air.local>
2025-10-15 10:47:38 +0700

    menambahkan path untuk media (storage menyimpan foto)

--- netly/settings.py
+++ netly/settings.py
@@ -51,7 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'community'
+    'community',
+    'authentication_user'
 ]
 
 MIDDLEWARE = [
@@ -150,6 +151,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# tambahan untuk media (nyimpen profile picture)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 if DEBUG:
     STATICFILES_DIRS = [
         BASE_DIR / 'static' 
