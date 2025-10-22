5d9318952eab62f7dfa5cea87e91c0d115513af7
ylbafle <cathlin.abigail@ui.ac.id>
2025-10-23 06:14:01 +0700

    update html

--- event/urls.py
+++ event/urls.py
@@ -2,21 +2,24 @@ from django.urls import path
 from event.views import (
     show_events,
     event_detail,
-    create_event,
-    edit_event,
-    delete_event,
     join_leave_event,
-    join_event_ajax
+    join_event_ajax,
+    create_event_ajax,
+    edit_event_ajax,
+    delete_event_ajax,
+    get_event_json,
 )
 
 app_name = 'event'
 
 urlpatterns = [
     path('', show_events, name='show_events'),
-    path('create/', create_event, name='create_event'),
     path('<uuid:pk>/', event_detail, name='event_detail'),
-    path('<uuid:pk>/edit/', edit_event, name='edit_event'),
-    path('<uuid:pk>/delete/', delete_event, name='delete_event'),
     path('<uuid:pk>/join/', join_leave_event, name='join_leave_event'),
     path('ajax/join/<uuid:pk>/', join_event_ajax, name='join_event_ajax'),
+    
+    path('ajax/create/', create_event_ajax, name='create_event_ajax'),
+    path('ajax/edit/<uuid:pk>/', edit_event_ajax, name='edit_event_ajax'),
+    path('ajax/delete/<uuid:pk>/', delete_event_ajax, name='delete_event_ajax'),
+    path('ajax/get/<uuid:pk>/', get_event_json, name='get_event_json'),         # untuk fetch data ke form edit
 ]
