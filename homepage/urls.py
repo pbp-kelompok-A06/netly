4824ce1d2553e3a7eea5d241a8f1367734b4e7d9
khalisaadzkiyah <khalisa.adzkiyah@ui.ac.id>
2025-10-23 18:42:49 +0700

    Update homepage changes

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='homepage'),
+    path("court/<int:court_id>/", views.court_detail, name="court-detail"),
+    path('booking/<int:court_id>/', views.booking_placeholder, name='booking-placeholder'),
+    path('search-courts/', views.search_courts_ajax, name='search-courts-ajax'),
+    path("filter-courts/", views.filter_courts, name="filter-courts"),
 ]
+
