4cac74e43cb499ba7564561afed64b596325d40a
Shotheaven <evan@Shotheavens-MacBook-Air.local>
2025-10-23 18:19:32 +0700

    nambahin make-admin biar bisa bikin admin

--- authentication_user/urls.py
+++ authentication_user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from authentication_user.views import login_view, logout_view, register_view, login_ajax, register_ajax
+from authentication_user.views import login_view, logout_view, register_view, login_ajax, register_ajax, make_admin
 
 
 app_name = 'authentication_user'
@@ -10,5 +10,6 @@ urlpatterns = [
     path('logout/', logout_view, name="logout"),
     path('register/', register_view, name="register"),
     path('register-ajax/', register_ajax, name="register_ajax"),
-    path('login-ajax/', login_ajax, name="login_ajax")
+    path('login-ajax/', login_ajax, name="login_ajax"),
+    path('make-admin', make_admin, name="make_admin")
 ]
